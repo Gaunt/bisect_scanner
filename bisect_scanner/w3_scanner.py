c37e7c1d78541b0753234e28dca9b4d0f2b90c73
Karel Novak <novakk5@gmail.com>
2022-03-15 20:28:35 +0100

    w3_scanner.py

--- bisect_scanner/w3_scanner.py
+++ bisect_scanner/w3_scanner.py
@@ -9,22 +9,27 @@ from bisect_scanner.base_scanner import BaseScanner
 load_dotenv()
 
 
-URL = os.environ('W3_URL', '')
+W3_URL = os.environ('W3_URL', '')
 DECIMALS = 18
 DELAY = 0.0
+CACHE_SIZE = 10000
 
 
 class W3Scanner(BaseScanner):
 
-    def __init__(self, account, w3: Union[str, Web3] = URL, *args, **kwargs):
+    def __init__(self, account, w3: Union[str, Web3] = W3_URL, *args, **kwargs):
         if isinstance(w3, str):
             self.web3 = Web3(Web3.WebsocketProvider(w3))
         else:
             self.web3 = w3
+
+        if not self.web3.is_connected():
+            raise ValueError('w3 not connected')
+
         self.account = account
         super().__init__(*args, **kwargs)
 
-    @lru_cache(10000)
+    @lru_cache(CACHE_SIZE)
     def block_balance(self, block):
         time.sleep(DELAY)
         balance = round(
@@ -38,3 +43,13 @@ class W3Scanner(BaseScanner):
 
     def w3(self):
         return self.web3
+
+
+def EthereumScanner(account):
+    ETHEREUM_URL = os.environ('ETHEREUM_URL', '')
+    return W3Scanner(account, ETHEREUM_URL)
+
+
+def PolygonScanner(account):
+    POLYGON_URL = os.environ('POLYGON_URL', '')
+    return W3Scanner(account, POLYGON_URL)
