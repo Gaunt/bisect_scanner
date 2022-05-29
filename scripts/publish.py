a47e866e7d103287612bf5735fbd755b9b3c777c
Karel Novak <novakk5@gmail.com>
2022-05-29 19:26:48 +0200

    publish script

---
+++ scripts/publish.py
@@ -0,0 +1,54 @@
+from subprocess import run
+import os
+import shlex
+from pathlib import Path
+from sys import version
+
+
+try:
+    basepath = (
+        (Path(os.path.dirname(__file__)) / Path("..")).absolute().resolve()
+    )
+except NameError:
+    basepath = (Path(os.getcwd()) / Path("..")).absolute().resolve()
+
+
+with open(basepath / "version.py") as f:
+    exec(f.read())
+    version = __version__
+
+
+def build():
+    for f in basepath.glob("dist/*"):
+        print(f'removing {f}')
+        os.remove(f)
+    setup_path = basepath / "setup.py"
+    run(shlex.split(f"python {setup_path} sdist bdist_wheel"), check=True)
+    run(shlex.split(f"twine check {basepath / 'dist'}/*"), check=True)
+
+
+def publish():
+    res = run(
+        f"twine upload --repository-url https://upload.pypi.org/legacy/ {basepath / 'dist'}/* --verbose",
+        shell=True,
+    )
+
+
+def tag():
+    tag_name = f"v{version}"
+    if len([*basepath.glob(f"dist/bisect_scanner-{version}*")]):
+        print(f"Build {version} exists, will be rebuilt")
+
+    if (
+        tag_name
+        in run(["git", "tag"], capture_output=True, encoding="utf-8").stdout
+    ):
+        print(f"Tag {tag_name} exists")
+    else:
+        run(shlex.split(f"git tag {tag_name}"), check=True)
+
+
+if __name__ == "__main__":
+    tag()
+    build()
+    publish()
--- setup.py
+++ setup.py
@@ -2,9 +2,8 @@ from pathlib import Path
 from setuptools import find_packages
 from setuptools import setup
 from pathlib import Path
+from version import __version__
 import os
-
-__version__ = "0.1.10"
 
 
 try:
@@ -39,15 +38,10 @@ setup(
     packages=find_packages(exclude=("tests",)),
     install_requires=reqs,
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
 )
